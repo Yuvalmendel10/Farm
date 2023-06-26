ac0fdd7b3e5f802bfa031bdb22b20cdd8a7621d8
Yuval Mendelovitz <yuvalmen10@gmail.com>
2023-06-26 20:01:22 +0300

    adding uuid

--- HorseFarm/src/App.tsx
+++ HorseFarm/src/App.tsx
@@ -1,25 +1,20 @@
-import { Card } from "@mui/material";
 import { useState } from "react";
-import { useTranslation } from "react-i18next";
 import "./App.css";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import kidWithHorse from "./assets/kidhowrse.jpg";
 import IActivity from "./interfaces/IActivity";
+import Activity from "./Components/activity/Activity";
+import { v4 } from "uuid";
 
 function App() {
-  const { t } = useTranslation();
   const [activities, setActivities] = useState<IActivity[]>([
     {
-      id: 1,
+      id: v4(),
       name: "camp",
       image: kidWithHorse,
       description: "campDescription",
     },
     {
-      id: 2,
+      id: v4(),
       name: "ridingTraining",
       image: kidWithHorse,
       description: "ridingTrainingDescription",
@@ -29,24 +24,7 @@ function App() {
   return (
     <div className="activities">
       {activities.map((activity) => (
-        <Card className="activity" sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={activity.image}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h3" component="div">
-                {t(activity.name)}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {t(activity.description)}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <Activity activity={activity} />
       ))}
     </div>
   );
