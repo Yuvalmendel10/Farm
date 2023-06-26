fd2e0038bf02feb07302605265f1d2afc5f12cce
Yuval Mendelovitz <yuvalmen10@gmail.com>
2023-06-26 19:20:13 +0300

    changing the first page

--- HorseFarm/src/App.tsx
+++ HorseFarm/src/App.tsx
@@ -1,16 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Card } from "@mui/material";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "./App.css";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import kidWithHorse from "./assets/kidhowrse.jpg";
+import IActivity from "./interfaces/IActivity";
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const { t } = useTranslation();
+  const [activities, setActivities] = useState<IActivity[]>([
+    {
+      id: 1,
+      name: "camp",
+      image: kidWithHorse,
+      description: "campDescription",
+    },
+    {
+      id: 2,
+      name: "ridingTraining",
+      image: kidWithHorse,
+      description: "ridingTrainingDescription",
+    },
+  ]);
 
   return (
-    <>
-     gts
-    </>
-  )
+    <div className="activities">
+      {activities.map((activity) => (
+        <Card className="activity" sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={activity.image}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h3" component="div">
+                {t(activity.name)}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {t(activity.description)}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ HorseFarm/src/interfaces/IActivity.ts
@@ -0,0 +1,6 @@
+export default interface IActivity {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+}
